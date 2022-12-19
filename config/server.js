9c16aaf898d82805340399c7a5b9826619ccfd19
Vadim Basko <vadim.p.basko@gmail.com>
2022-12-19 13:05:24 +0400

    init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '8c734eb351c721c5095c186e0bf4ec0a'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '66c13862-fb37-4250-9f7d-babb5fb2a030'
+};
